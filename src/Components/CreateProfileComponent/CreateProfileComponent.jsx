ea18a0dbffbdd5202d6da77247205ee8298b3563
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2022-08-15 01:53:01 +0700

    create component validation form success

--- src/Components/CreateProfileComponent/CreateProfileComponent.jsx
+++ src/Components/CreateProfileComponent/CreateProfileComponent.jsx
@@ -1,86 +1,135 @@
-import React from 'react'
-import './CreateProfileComponent.css'
+import React from "react";
+import "./CreateProfileComponent.css";
+import FileBase from "react-file-base64";
+import { useState, useRef } from "react";
 
 function CreateProfileEditProfileComponent() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    weight: 0,
+    height: 0,
+    age: 0,
+    gender: "female",
+    picture: "",
+  });
+  const refConfirmPassword = useRef()
+  const refPassword = useRef()
+  const divShowErrorPass = useRef()
+  const divShowErrorConfirm = useRef()
+
+//เก็บของมาจาก input ทั้งหน้า
+  function handleChange(event) {
+    setFormData({...formData,[event.target.name]: event.target.value})
+  }
+  function handleOnSubmit(event) {
+    event.preventDefault()
+    if (confirmPasswordCheck()) {
+      // send to backend database
+      clearError()
+    } else {
+      setError()
+    }
+  }
+  function setError(){
+    refPassword.current.focus()
+    refConfirmPassword.current.style.backgroundColor = '#e85757'
+    refPassword.current.style.backgroundColor = '#e85757'
+
+    divShowErrorPass.current.className = 'wrong-password'
+    divShowErrorConfirm.current.className = 'wrong-password'
+  }
+  function clearError(){
+    refConfirmPassword.current.style.backgroundColor = 'white'
+    refPassword.current.style.backgroundColor = 'white'
+
+    divShowErrorPass.current.className = ''
+    divShowErrorConfirm.current.className = ''
+  }
+  function confirmPasswordCheck() {
+    const confirmPassword = refConfirmPassword.current.value
+    return formData.password === confirmPassword
+  }
+  
+  console.log(formData)
+
   return (
-    <div className='Profile-user-container'>
-        
-        <div className='profile-create-user'>
-          <h2>Create Account</h2>
-              <form action="" className='form-create-profile'>
+    <div className="Profile-user-container">
+      <div className="profile-create-user">
+        <h2>Create Account</h2>
+        <form onSubmit={handleOnSubmit} className="form-create-profile">
+          <div className="create-p-label">
+            <label htmlFor="name">NAME</label>
+          </div>
+          <div className="create-p-input">
+            <input onChange={handleChange} type="text" id="name" name="name" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="name">NAME</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type="text" id='name' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="email">E-MAIL</label>
+          </div>
+          <div className="create-p-input">
+            <input onChange={handleChange} type="email" id="email" name="email" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="email">E-MAIL</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type='email' id='email' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="password">PASSWORD</label>
+          </div>
+          <div  className="create-p-input">
+            <div ref={divShowErrorPass} ></div>
+            <input ref={refPassword} onChange={handleChange} type="password" id="password" name="password" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="password">PASSWORD</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type="number" id='password' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="pwconfirm">CONFIRM PASSWORD</label>
+          </div>
+          <div  className="create-p-input">
+            <div ref={divShowErrorConfirm} ></div>
+            <input ref={refConfirmPassword} onChange={confirmPasswordCheck} type="password" id="pwconfirm" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="pwconfirm">CONFIRM PASSWORD</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type="number" id='pwconfirm' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="weight">WEIGHT</label>
+          </div>
+          <div className="create-p-input">
+            <input onChange={handleChange} type="number" id="weight" name="weight" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="weight">WEIGHT</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type="number" id='weight' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="height">HEIGHT</label>
+          </div>
+          <div className="create-p-input">
+            <input onChange={handleChange} type="number" id="height" name="height" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="height">HEIGHT</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type="number" id='height' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="age">AGE</label>
+          </div>
+          <div className="create-p-input">
+            <input onChange={handleChange} type="number" id="age" name="age" required />
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="age">AGE</label>
-                </div>
-                <div className='create-p-input'>
-                  <input type="number" id='age' required />
-                </div>
+          <div className="create-p-label">
+            <label htmlFor="gender">GENDER</label>
+          </div>
+          <div className="create-p-gender">
+            <select onChange={handleChange} name="gender" id="gender">
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+            </select>
+          </div>
 
-                <div className='create-p-label'>
-                  <label htmlFor="gender">GENDER</label>
-                </div>
-                <div className='create-p-gender'>
-                <select name='gender' id='gender'>
-                  <option value="woman">WOMAN</option>
-                  <option value="man">MAN</option>
-                </select>
-                </div>
-
-                <div className='create-p-label-load'>
-                  <button htmlFor="upload">Click Upload Profile Pic</button>
-                </div>
-                <div className='create-p-input'>
-                  <input type="url" id='upload' required />
-                </div>
-                <div className='btn-create-profile'>
-              <button >Create Now</button>
-              </div>
-            </form>
-        </div>
+          <div className="create-p-label-load">
+            <FileBase onDone={(filePic)=>setFormData({...formData,picture: filePic.base64})} type="file" multiple={false} />
+          </div>
+          <div className="btn-create-profile">
+            <button type="submit">Create Now</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CreateProfileEditProfileComponent;
