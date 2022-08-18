d86fd66ee61d80ce7b309145f03dca675cb15062
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2022-08-18 18:14:10 +0700

    create blank initial instead using mockupdata.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,62 @@ import CreateProfilePage from "./Pages/CreateProfilePage/CreateProfilePage";
 import EditProfilePage from "./Pages/EditProfilePage/EditProfilePage";
 import FullRunDiaryPage from "./Pages/FullRunDiaryPage/FullRunDiaryPage";
 import CreateRunDiaryPage from "./Pages/CreateRunDiaryPage/CreateRunDiaryPage";
-import { useState, createContext } from "react";
-import initialState from "./mockupdata";
+import { useState, createContext, useEffect } from "react";
+import axios from "axios";
 
-export const DataContext = createContext(null);
+export const DataContext = createContext();
 
 function App() {
-  const [data, setData] = useState(initialState);
+
+  const [forceRender, setForceRender] = useState(true)
+
+  const [userID, setUserID] = useState("");
+  // const userID = JSON.parse(localStorage.getItem('rundev'))
+
+  const [data, setData] = useState({
+    auth: {
+      email: "",
+      password: "",
+    },
+    bio: {
+      name: "",
+      weight: 0,
+      height: 0,
+      age: 0,
+      gender: "",
+      picture: "",
+      bmi: 0,
+    },
+    token: "",
+    rundiary: [],
+  });
+  useEffect(() => {
+    (async () => {
+      const users = await axios.get(
+        `${import.meta.env.VITE_baseURL}/user/62f93f05129b0cce4860c897`
+      );
+      const reIdrundiary = users.data.rundiary.map((rundiary) => 
+      (
+        {
+        id: rundiary._id,
+        ...rundiary,
+        date: new Date(rundiary.date).toLocaleDateString(),
+      }
+      )
+
+      );
+
+      setUserID(users.data._id);
+      // localStorage.setItem('rundev',JSON.stringify(users.data._id))
+      setData({ ...users.data, rundiary: reIdrundiary });
+    })();
+  }, [forceRender]);
+
+  function toggleForceRender(){
+    setForceRender((prev)=>{
+      return !prev
+    })
+  }
 
   // create Run Diary (+)
   function createRunDiary(newCard) {
@@ -22,37 +71,35 @@ function App() {
 
   // create function BMI
   function calculateBMI(weight, height) {
-    const convertHeight = (height/100)**2;
-    const bmi = (weight/convertHeight).toFixed(2);
-    return bmi
+    const convertHeight = (height / 100) ** 2;
+    const bmi = (weight / convertHeight).toFixed(2);
+    return bmi;
   }
   // End create function BMI
 
   // create function delete btn at home (-)
   function deleteDiaryCard(id) {
-    const newRundiary = data.rundiary.filter((cardRundiary)=> cardRundiary.id !== id)
-    setData({...data,rundiary:newRundiary})
+    const newRundiary = data.rundiary.filter(
+      (cardRundiary) => cardRundiary.id !== id
+    );
+    setData({ ...data, rundiary: newRundiary });
   }
   // End create function delete btn at home (-)
   const context = {
     data,
     createRunDiary,
     calculateBMI,
-    deleteDiaryCard
-  }
+    deleteDiaryCard,
+    userID,
+    toggleForceRender,
+  };
   return (
     <DataContext.Provider value={context}>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={<HomePage />}
-          />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/createprofile"
-            element={<CreateProfilePage />}
-          />
+          <Route path="/createprofile" element={<CreateProfilePage />} />
           <Route path="/editprofile" element={<EditProfilePage />} />
           <Route path="/rundiary" element={<FullRunDiaryPage />} />
           <Route path="/createrundiary" element={<CreateRunDiaryPage />} />
